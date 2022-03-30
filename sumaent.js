36695fc9a996e55cb34ba3a0fbdbf300c61a2c99
Teresa Miguel <98185118+tere79purple@users.noreply.github.com>
2022-03-30 15:21:09 -0600

    primer commit
    archivos html y sumaent.js

---
+++ sumaent.js
@@ -0,0 +1,13 @@
+//suma de sos enteros
+
+var i=0;
+var numero=0;
+var misuma=0;
+   while(i<10){
+  if(numero%2==0){
+  misuma=misuma+numero;
+  i=i+1;
+}
+numero=numero+1;
+}
+document.write(misuma);
